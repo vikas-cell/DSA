1222b3d7f4b3f3c807a407e003c64d5ab57e506b
vikas-cell <vikaskaushal1510@gmail.com>
2025-07-05 09:59:54 +0530

    Time: 9 ms (94.33%) | Memory: 49 MB (34.56%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,38 +1,50 @@
 class Solution {
+    public class Pair implements Comparable<Pair>{
+        int node;
+        int time;
+        Pair(int node,int time){
+            this.node = node;
+            this.time = time;
+        }
+
+        public int compareTo(Pair p){
+            return this.time-p.time;
+        }
+    }
     public int networkDelayTime(int[][] times, int n, int k) {
-        ArrayList<ArrayList<int []>> adj = new ArrayList<>();
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
         for(int i=0;i<=n;i++) adj.add(new ArrayList<>());
 
         for(int i=0;i<times.length;i++){
             int u = times[i][0];
             int v = times[i][1];
-            int t = times[i][2];
-            adj.get(u).add(new int[]{v,t});
+            int time = times[i][2];
+
+            adj.get(u).add(new Pair(v,time));
         }
 
         int [] dist = new int[n+1];
         Arrays.fill(dist,Integer.MAX_VALUE);
-        PriorityQueue<int []> pq = new PriorityQueue<>((a,b)->Integer.compare(a[1],b[1]));
-        pq.offer(new int[]{k,0});
         dist[k] = 0;
-        
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.offer(new Pair(k,0));
+
         while(pq.size()>0){
-            int [] p = pq.poll();
-            int u = p[0];
-            int dist_u = p[1];
-            if(dist_u>dist[u]) continue;
-            for(int [] mat:adj.get(u)){
-                int v = mat[0];
-                int cost = mat[1];
-                if(dist_u+cost<dist[v]){
-                    dist[v] = dist_u+cost;
-                    pq.offer(new int[]{v,dist[v]});
+            Pair p = pq.poll();
+            int u = p.node;
+            int dist_u = p.time;
+            for(Pair curr:adj.get(u)){
+                int v = curr.node;
+                int cost = curr.time;
+                if(cost+dist_u<dist[v]){
+                    dist[v] = cost+dist_u;
+                    pq.offer(new Pair(v,dist[v]));
                 }
             }
         }
-            int ans = Integer.MIN_VALUE;
-            for(int i=1;i<=n;i++) ans = Math.max(ans,dist[i]);
-            if(ans==Integer.MAX_VALUE) return -1;
-            return ans;
+        int ans = -1;
+        for(int i=1;i<=n;i++) ans = Math.max(ans,dist[i]);
+        if(ans==Integer.MAX_VALUE) return -1;
+        return ans;
     }
 }

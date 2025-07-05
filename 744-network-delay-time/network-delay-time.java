e3e75ddbc659ac2597b4121212a4e7572aa2180a
vikas-cell <vikaskaushal1510@gmail.com>
2025-07-05 09:27:33 +0530

    Time: 10 ms (83.25%) | Memory: 49.4 MB (16.12%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,36 +1,38 @@
 class Solution {
-    public int networkDelayTime(int[][] edges, int V, int src) {
-         ArrayList<ArrayList<int []>> al = new ArrayList<>();
-        for(int i=0;i<V;i++) al.add(new ArrayList<>());
-        for(int i=0;i<edges.length;i++){
-            int a = edges[i][0];
-            int b = edges[i][1];
-            int c = edges[i][2];
-            al.get(a-1).add(new int[]{b-1,c});
-            // al.get(b-1).add(new int[]{a-1,c});
+    public int networkDelayTime(int[][] times, int n, int k) {
+        ArrayList<ArrayList<int []>> adj = new ArrayList<>();
+        for(int i=0;i<=n;i++) adj.add(new ArrayList<>());
+
+        for(int i=0;i<times.length;i++){
+            int u = times[i][0];
+            int v = times[i][1];
+            int t = times[i][2];
+            adj.get(u).add(new int[]{v,t});
         }
-        int [] dist = new int[V];
-        for(int i=0;i<V;i++) dist[i] = Integer.MAX_VALUE;
-        dist[src-1] = 0;
-        PriorityQueue<int []> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
-        pq.offer(new int[]{src-1,0});
+
+        int [] dist = new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        PriorityQueue<int []> pq = new PriorityQueue<>((a,b)->Integer.compare(a[1],b[1]));
+        pq.offer(new int[]{k,0});
+        dist[k] = 0;
         
         while(pq.size()>0){
             int [] p = pq.poll();
             int u = p[0];
-            int d_u = p[1];
-            for(int [] mat:al.get(u)){
+            int dist_u = p[1];
+
+            for(int [] mat:adj.get(u)){
                 int v = mat[0];
                 int cost = mat[1];
                 if(dist[u]+cost<dist[v]){
                     dist[v] = dist[u]+cost;
-                    pq.offer(new int[]{v, dist[v]});
+                    pq.offer(new int[]{v,dist[v]});
                 }
             }
         }
-        int ans = Integer.MIN_VALUE;
-        for(int i=0;i<V;i++) ans = Math.max(ans,dist[i]);
-        if(ans==Integer.MAX_VALUE) return -1;
-        return ans;
+            int ans = Integer.MIN_VALUE;
+            for(int i=1;i<=n;i++) ans = Math.max(ans,dist[i]);
+            if(ans==Integer.MAX_VALUE) return -1;
+            return ans;
     }
 }

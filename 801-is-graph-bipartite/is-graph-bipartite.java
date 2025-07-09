744c4782c62c3223e4edbe4a6a0662480e2be6ec
vikas-cell <vikaskaushal1510@gmail.com>
2025-07-09 10:01:19 +0530

    Time: 2 ms (32.73%) | Memory: 45.1 MB (52.69%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,36 +1,52 @@
 class Solution {
-    public boolean ans;
-    public boolean isBipartite(int[][] adj) {
-       int n = adj.length;
-       ans = true;
-       int [] visit = new int[n];
-       Arrays.fill(visit,-1);
-       for(int i=0;i<n;i++){
-        if(visit[i]==-1){
-            if(ans==false) return ans;
-            bfs(i,visit,adj);
-        }
-       }
-       return ans;
+    public int parent[];
+    public int size[];
+    public boolean pairity[];
+    public int find(int u){
+        if(parent[u]==u) return u;
+        return parent[u] = find(parent[u]);
     }
-    public void bfs(int s,int [] visit,int [][] adj){
-        visit[s] = 0;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(s);
+    public void union(int u,int v){
+        int a = find(u);
+        int b = find(v);
+        if(a!=b){
+            if(size[a]>size[b]){
+                parent[b] = a;
+                size[a]+=size[b];
+                pairity[v] = !pairity[u];
+            }
+            else{
+                parent[a] = b;
+                size[b]+=size[a];
+                pairity[u] = !pairity[v];
+            }
+        }
+    }
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        parent = new int[n];
+        size = new int[n];
+        pairity = new boolean[n];
+        
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+            size[i] = 1;
+            pairity[i] = false;
+        }
 
-        while(q.size()>0){
-            int front = q.poll();
-            int color = visit[front];
-            for(int elem:adj[front]){
-                if(visit[elem]==visit[front]){
-                    ans = false;
-                    return;
-                }
-                if(visit[elem]==-1){
-                    q.add(elem);
-                    visit[elem] = 1-color;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<graph[i].length;j++){
+                int u = i; int v = graph[i][j];
+                if(v>u){
+                    if(find(u)==find(v)){
+                        if(pairity[u]==pairity[v]) return false;
+                    }
+                    else{
+                        union(u,v);
+                    }
                 }
             }
         }
+        return true;
     }
 }

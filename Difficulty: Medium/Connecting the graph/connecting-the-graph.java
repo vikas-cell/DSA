55f58cc18c96cc1d93b577661b3dbf4f2725576d
vikas-cell <vikaskaushal1510@gmail.com>
2025-07-09 10:56:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Connecting the graph/connecting-the-graph.java
@@ -0,0 +1,52 @@
+class Solution {
+    public static int [] size;
+    public static int [] parent;
+    
+    public static int find(int u){
+        if(parent[u]==u) return u;
+        return parent[u] = find(parent[u]);
+    }
+    
+    public static void union(int u,int v){
+        int a = find(u);
+        int b = find(v);
+        
+        if(a!=b){
+            if(size[a]>size[b]){
+                parent[b] = a;
+                size[a]+=size[b];
+            }
+            else{
+                parent[a] = b;
+                size[b]+=size[a];
+            }
+        }
+    }
+    public int Solve(int n, int[][] edge) {
+        // Code here
+        int m = edge.length;
+        int extracount = 0;
+        parent = new int[n];
+        size = new int[n];
+        
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+        
+        for(int i=0;i<m;i++){
+            int u = edge[i][0]; int v = edge[i][1];
+            if(find(u)!=find(v)){
+                union(u,v);
+            }
+            else extracount++;
+         }
+         
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(parent[i]==i) count++;
+        }
+        if(extracount>=count-1) return count-1;
+        return -1;
+    }
+}

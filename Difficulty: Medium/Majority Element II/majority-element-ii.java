b24481ab1fd9a9253d8c9b109cc47e48839ccd03
vikas-cell <vikaskaushal1510@gmail.com>
2025-07-26 10:19:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element II/majority-element-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ArrayList<Integer> findMajority(int[] arr) {
+        // Code here
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = arr.length;
+        ArrayList<Integer> al = new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            int elem = arr[i];
+            if(map.containsKey(arr[i])){
+                map.put(elem,map.get(elem)+1);
+            }
+            else map.put(elem,1);
+        }
+        
+        for(int key:map.keySet()){
+            int freq = map.get(key);
+            if(freq>n/3) al.add(key);
+        }
+        Collections.sort(al);
+        return al;
+    }
+}

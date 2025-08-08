aae54845dc694999fca9e7e8c6a48263441863d3
vikas-cell <vikaskaushal1510@gmail.com>
2025-08-08 09:27:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.java
@@ -0,0 +1,25 @@
+class Solution {
+    int getLPSLength(String s) {
+        int n = s.length();
+        int[] lps = new int[n];
+        int len = 0; // length of the previous longest prefix suffix
+        int i = 1;
+
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps[n - 1]; // length of longest prefix which is also suffix
+    }
+}

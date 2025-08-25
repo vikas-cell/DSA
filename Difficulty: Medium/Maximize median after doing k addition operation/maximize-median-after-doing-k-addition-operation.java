54de64a95dd4c622323bf6623660108b6a1e9bc9
vikas-cell <vikaskaushal1510@gmail.com>
2025-08-25 10:35:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximize median after doing k addition operation/maximize-median-after-doing-k-addition-operation.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maximizeMedian(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        Arrays.sort(arr);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        int i = 0;
+        if(n%2==0) i = (n/2)-1;
+        else i = n/2;
+        
+        while(i<n){
+            pq.add(arr[i]);
+            i++;
+        }
+        
+        while(k>0){
+            int elem = pq.poll();
+            pq.add(elem+1);
+            k--;
+        }
+        if(n%2==0) return (pq.poll()+pq.poll())/2;
+        return pq.poll();
+    }
+}

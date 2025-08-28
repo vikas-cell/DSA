a311b98fde6729144808658622348aff86666334
vikas-cell <vikaskaushal1510@gmail.com>
2025-08-28 10:21:52 +0530

    Time: 3 ms (94.50%) | Memory: 45.6 MB (57.80%) - LeetSync

---
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[][] sortMatrix(int[][] grid) {
+        
+        int n = grid.length;
+
+        // ArrayList<ArrayList<Integer>> al = new ArrayList<>();
+        // for(int i=0;i<n;i++) al.add(new ArrayList<>());
+
+        int count = 1;
+        while(count<n){
+            ArrayList<Integer> al = new ArrayList<>();
+            int i=0;int j=count;
+            while(i<n && j<n){
+                al.add(grid[i][j]);
+                i++; j++;
+            }
+            Collections.sort(al);
+            i=0;j=count; int k = 0;
+            while(i<n && j<n){
+                grid[i][j] = al.get(k++);
+                i++; j++;
+            }
+            count++;
+        }
+        
+        count = 0;
+        while(count<n){
+            ArrayList<Integer> al = new ArrayList<>();
+            int i=count;int j=0;
+            while(i<n && j<n){
+                al.add(grid[i][j]);
+                i++; j++;
+            }
+            Collections.sort(al);
+            i=count; j=0;int k=al.size()-1;
+
+            while(i<n && j<n){
+                grid[i][j] = al.get(k--);
+                i++;j++;
+            }
+            count++;
+        }
+     return grid;
+    }
+}
